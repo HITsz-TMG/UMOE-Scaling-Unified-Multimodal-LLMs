b243bb1c0d439b2a0f0a3c856985365e63e6962e
expapa <45393746+expapa@users.noreply.github.com>
2024-05-31 10:13:30 +0800

    Update eval_speech.sh

--- Uni_MoE_v2/eval_speech.sh
+++ Uni_MoE_v2/eval_speech.sh
@@ -11,12 +11,12 @@ deepspeed --num_gpus=2 --num_nodes=1 \
     Uni_MoE_speech/eval.py \
     --deepspeed ./scripts/zero2.json \
     --model_base checkpoints/Uni-MoE-speech-base \
-    --model_path output/Uni_MoE_speech_8moe \
+    --model_path output/Uni_MoE_v2_e2 \
     --data_path path/to/eval.json \
     --enable_deepspeed_moe True \
     --data_type vqa\
     --eval_ep_size 2 \
-    --mlp_dir path/to/MoeMerge\
+    --mlp_dir path/to/Uni-MoE-v2-Experts\
     --version v1 \
     --vision_tower checkpoints/clip-vit-large-patch14-336 \
     --audio_tower checkpoints/whisper-small \
