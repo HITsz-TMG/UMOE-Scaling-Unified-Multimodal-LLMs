f756021e17c14a3e563eb2ac03b2dce28106c451
expapa <45393746+expapa@users.noreply.github.com>
2024-05-31 10:13:59 +0800

    Update inference_speech.sh

--- Uni_MoE_v2/inference_speech.sh
+++ Uni_MoE_v2/inference_speech.sh
@@ -12,12 +12,12 @@ deepspeed --num_gpus=2 --num_nodes=1 \
     Uni_MoE_speech/inference_new.py \
     --deepspeed ./scripts/zero2.json \
     --model_base path/to/Uni-MoE-speech-base \
-    --model_path output/Uni_MoE_speech_test_final \
+    --model_path output/Uni_MoE_v2_e2 \
     --data_path /path/to/eval.json \
     --enable_deepspeed_moe True \
     --data_type vqa\
     --eval_ep_size 2 \
-    --mlp_dir path/to/MoeMerge\
+    --mlp_dir path/to/Uni_MoE_v2_Experts\
     --version v1 \
     --vision_tower path/to/clip-vit-large-patch14-336 \
     --audio_tower path/to/whisper-small \
